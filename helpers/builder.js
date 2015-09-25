a2364d7e497121d66d90b4c5ec190175f6db45cf
Colin O'Brien <obrien.colin@gmail.com>
2015-09-24 22:58:48 -0400

    Wrapping the contents of the g:evaluate inside a CDATA tag

--- helpers/builder.js
+++ helpers/builder.js
@@ -6,7 +6,9 @@ var build = function (data, html) {
   return `<?xml version="1.0" encoding="utf-8" ?>
 <j:jelly trim="false" xmlns:j="jelly:core" xmlns:g="glide" xmlns:j2="null" xmlns:g2="null">
   <g:evaluate var="jvar_data" jelly="true" object="true">
-    ${data}
+    <![CDATA[
+      ${data}
+    ]]>
   </g:evaluate>
 
   ${html}
