3a9e77e7615595ac80cdf1694c2a03684bf8b059
Colin O'Brien <obrien.colin@gmail.com>
2015-10-02 10:47:58 -0400

    Fixing merge issue

--- helpers/builder.js
+++ helpers/builder.js
@@ -5,17 +5,11 @@ var File = gutil.File;
 var build = function (data, html) {
   return `<?xml version="1.0" encoding="utf-8" ?>
 <j:jelly trim="false" xmlns:j="jelly:core" xmlns:g="glide" xmlns:j2="null" xmlns:g2="null">
-<<<<<<< HEAD
-  <g:evaluate var="jvar_data" jelly="true" object="true"><[CDATA[
-    ${data}
-  ]]></g:evaluate>
-=======
   <g:evaluate var="jvar_data" jelly="true" object="true">
     <![CDATA[
       ${data}
     ]]>
   </g:evaluate>
->>>>>>> a2364d7e497121d66d90b4c5ec190175f6db45cf
 
   ${html}
 </j:jelly>
